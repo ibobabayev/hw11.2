e7c2d72761749c8e708b6715bd9dd58e0478b93d
ibobabayev <ibrahim42babayev@gmail.com>
2023-11-17 19:54:45 +0300

    docstring + new func added

--- str_func.py
+++ str_func.py
@@ -2,6 +2,6 @@ def func(word):
     """делает заглавными все буквы каждого слова в строке"""
     return word.upper()
 
-def func2(string):
+def func2(word):
     """делает заглавными первые буквы каждого слова в строке"""
-    return string.title()
+    return word.title()
